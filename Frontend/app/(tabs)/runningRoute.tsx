b1947359aa04da8db3a273303083837e15b4e286
qfqf <e1121168@u.nus.edu>
2024-06-29 14:33:39 +0800

    updated skeleton

---
+++ Frontend/app/(tabs)/runningRoute.tsx
@@ -0,0 +1,41 @@
+import { WebView } from "react-native-webview";
+import { StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { getItem } from "@/components/general/asyncStorage";
+
+const ip = process.env.EXPO_PUBLIC_DOMAIN;
+
+export default function runningRoute() {
+  const [token, setToken] = useState<string | null>(null);
+  useEffect(() => {
+    const fetchToken = async () => {
+      const storedToken = await getItem("token");
+      setToken(storedToken);
+    };
+
+    fetchToken();
+  }, []);
+
+  return token ? (
+    <WebView
+      style={styles.container}
+      javaScriptEnabled={true}
+      startInLoadingState={true}
+      source={{
+        uri: `http://${ip}:8000/map`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+      }}
+    />
+  ) : (
+    <></>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
