71a4a540cfce92bc33c78cc81a327974aae3b979
NgZiXin <ngzixin02@gmail.com>
2024-07-01 09:01:52 +0800

    Check Authentication

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,8 +1,13 @@
 import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { getItem } from "@/components/general/asyncStorage";
 
 // Expo router first routes to this page (entry point)
 // This page then redirects to login page
 const Index = () => {
-  return <Redirect href="/login" />;
+  const [token, setToken] = useState<string | null>(null)
+  // Check if authenticated
+  useEffect(() => {getItem("token").then(item => setToken(item))},[])
+  return token ? <Redirect href="/(tabs)/profile" /> : <Redirect href="/login" />;
 };
 export default Index;
