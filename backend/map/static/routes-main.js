bf6df6719223f0976ac96a5657d3fe9754c4bd32
NgZiXin <ngzixin02@gmail.com>
2024-08-10 18:04:28 +0800

    Temporary bug fix for maps

--- backend/map/static/routes-main.js
+++ backend/map/static/routes-main.js
@@ -336,51 +336,54 @@ function clickWaypoint(e, marker) {
 
   // Popup "delete" button
   // TODO: In future use event delegation
-  document
-    .getElementById("marker-delete-button")
-    .addEventListener("click", () => {
-      // Close Popup
-      map.closePopup(popup);
+  // Currently use a temporary timeout solution (NOT RELIABLE)
+  setTimeout(() => {
+    document
+      .getElementById("marker-delete-button")
+      .addEventListener("click", () => {
+        // Close Popup
+        map.closePopup(popup);
 
-      const prevWaypoint = prev.get(marker);
-      const nextWaypoint = next.get(marker);
+        const prevWaypoint = prev.get(marker);
+        const nextWaypoint = next.get(marker);
 
-      // Join previous and next waypoints together
-      if ((prevWaypoint != undefined) & (nextWaypoint != undefined)) {
-        buildRoute(
-          prevWaypoint,
-          nextWaypoint,
-          false,
-          false,
-          marker.options.connectedLines[0].options.order // Maintain order
-        );
-      }
-
-      // Update hashMap
-      if (prevWaypoint) {
-        next.set(prevWaypoint, nextWaypoint);
-      }
-      if (nextWaypoint) {
-        prev.set(nextWaypoint, prevWaypoint);
-      }
-      prev.delete(marker);
-      next.delete(marker);
-
-      // Update prevMarker
-      if (prevMarker == marker) {
-        prevMarker = prevWaypoint;
-      }
-
-      // Delete marker and polylines from map
-      route.removeLayer(marker);
-      marker.options.connectedLines.forEach((line) => {
-        if (route.hasLayer(line)) {
-          route.removeLayer(line);
-          removeItemOnce(routeArr, line);
-          totalDistance -= line.getDistance();
+        // Join previous and next waypoints together
+        if ((prevWaypoint != undefined) & (nextWaypoint != undefined)) {
+          buildRoute(
+            prevWaypoint,
+            nextWaypoint,
+            false,
+            false,
+            marker.options.connectedLines[0].options.order // Maintain order
+          );
         }
+
+        // Update hashMap
+        if (prevWaypoint) {
+          next.set(prevWaypoint, nextWaypoint);
+        }
+        if (nextWaypoint) {
+          prev.set(nextWaypoint, prevWaypoint);
+        }
+        prev.delete(marker);
+        next.delete(marker);
+
+        // Update prevMarker
+        if (prevMarker == marker) {
+          prevMarker = prevWaypoint;
+        }
+        
+        // Delete marker and polylines from map
+        route.removeLayer(marker);
+        marker.options.connectedLines.forEach((line) => {
+          if (route.hasLayer(line)) {
+            route.removeLayer(line);
+            removeItemOnce(routeArr, line);
+            totalDistance -= line.getDistance();
+          }
+        });
       });
-    });
+  }, 100);
 }
 
 function segmentQuery(e) {
@@ -477,55 +480,58 @@ function clickSegment(e, start, end, polyline, segmentName) {
 
   // Logic for adding of segments
   // TODO: In future use event delegation
-  document
-    .getElementById("polylineButton")
-    .addEventListener("click", function () {
-      // Close popup
-      map.closePopup(popup);
+  // Currently use a temporary timeout solution (NOT RELIABLE)
+  setTimeout(() => {
+    document
+      .getElementById("polylineButton")
+      .addEventListener("click", function () {
+        // Close popup
+        map.closePopup(popup);
 
-      // Build route to start point
-      if (prevMarker != undefined) {
-        const fitStart = prev.size == 0;
-        buildRoute(prevMarker, start, fitStart, false, orderCount);
+        // Build route to start point
+        if (prevMarker != undefined) {
+          const fitStart = prev.size == 0;
+          buildRoute(prevMarker, start, fitStart, false, orderCount);
+          orderCount += 1;
+        }
+
+        // Update hashMap
+        prev.set(end, start);
+        next.set(start, end);
+
+        // Update prevMarker
+        prevMarker = end;
+
+        // Edit polyline
+        polyline.off("click");
+        polyline.options.order = orderCount;
         orderCount += 1;
-      }
 
-      // Update hashMap
-      prev.set(end, start);
-      next.set(start, end);
+        // Layer management
+        stravaSegments.removeLayer(start);
+        stravaSegments.removeLayer(polyline);
+        route.addLayer(start);
+        route.addLayer(end);
+        route.addLayer(polyline);
 
-      // Update prevMarker
-      prevMarker = end;
+        // Add click event to markers
+        start.on("click", (e) =>
+          clickSegmentWaypoint(e, start, end, polyline, segmentName)
+        );
+        end.on("click", (e) =>
+          clickSegmentWaypoint(e, start, end, polyline, segmentName)
+        );
 
-      // Edit polyline
-      polyline.off("click");
-      polyline.options.order = orderCount;
-      orderCount += 1;
+        // Style marker and polyline
+        start.setStyle(selectSegmentPoint);
+        end.setStyle(selectSegmentPoint);
+        polyline.setStyle(selectPolyline);
 
-      // Layer management
-      stravaSegments.removeLayer(start);
-      stravaSegments.removeLayer(polyline);
-      route.addLayer(start);
-      route.addLayer(end);
-      route.addLayer(polyline);
-
-      // Add click event to markers
-      start.on("click", (e) =>
-        clickSegmentWaypoint(e, start, end, polyline, segmentName)
-      );
-      end.on("click", (e) =>
-        clickSegmentWaypoint(e, start, end, polyline, segmentName)
-      );
-
-      // Style marker and polyline
-      start.setStyle(selectSegmentPoint);
-      end.setStyle(selectSegmentPoint);
-      polyline.setStyle(selectPolyline);
-
-      // Update totalDistance and routeArr
-      routeArr.push(polyline);
-      totalDistance += polyline.getDistance();
-    });
+        // Update totalDistance and routeArr
+        routeArr.push(polyline);
+        totalDistance += polyline.getDistance();
+      });
+  }, 100);
 }
 
 function clickSegmentWaypoint(e, start, end, polyline, segmentName) {
@@ -558,59 +564,62 @@ function clickSegmentWaypoint(e, start, end, polyline, segmentName) {
 
   // Logic for removing of segments
   // TODO: In future use event delegation
-  document
-    .getElementById("removeButton")
-    .addEventListener("click", function () {
-      // Close Popup
-      map.closePopup(popup);
+  // Currently use a temporary timeout solution (NOT RELIABLE)
+  setTimeout(() => {
+    document
+      .getElementById("removeButton")
+      .addEventListener("click", function () {
+        // Close Popup
+        map.closePopup(popup);
 
-      const prevWaypoint = prev.get(start);
-      const nextWaypoint = next.get(end);
+        const prevWaypoint = prev.get(start);
+        const nextWaypoint = next.get(end);
 
-      // Join previous and next waypoints together
-      if ((prevWaypoint != undefined) & (nextWaypoint != undefined)) {
-        buildRoute(
-          prevWaypoint,
-          nextWaypoint,
-          false,
-          false,
-          start.options.connectedLines[0].options.order
-        );
-      }
-
-      // Update hashMap
-      if (prevWaypoint) {
-        next.set(prevWaypoint, nextWaypoint);
-      }
-      if (nextWaypoint) {
-        prev.set(nextWaypoint, prevWaypoint);
-      }
-      prev.delete(start);
-      next.delete(start);
-      prev.delete(end);
-      next.delete(end);
-
-      // Update prevMarker
-      if (prevMarker == end) {
-        prevMarker = prevWaypoint;
-      }
-
-      // Delete marker and polylines from map
-      map.removeLayer(start);
-      start.options.connectedLines.forEach((line) => {
-        if (route.hasLayer(line)) {
-          route.removeLayer(line);
-          removeItemOnce(routeArr, line);
-          totalDistance -= line.getDistance();
+        // Join previous and next waypoints together
+        if ((prevWaypoint != undefined) & (nextWaypoint != undefined)) {
+          buildRoute(
+            prevWaypoint,
+            nextWaypoint,
+            false,
+            false,
+            start.options.connectedLines[0].options.order
+          );
         }
-      });
-      map.removeLayer(end);
-      end.options.connectedLines.forEach((line) => {
-        if (route.hasLayer(line)) {
-          route.removeLayer(line);
-          removeItemOnce(routeArr, line);
-          totalDistance -= line.getDistance();
+
+        // Update hashMap
+        if (prevWaypoint) {
+          next.set(prevWaypoint, nextWaypoint);
         }
+        if (nextWaypoint) {
+          prev.set(nextWaypoint, prevWaypoint);
+        }
+        prev.delete(start);
+        next.delete(start);
+        prev.delete(end);
+        next.delete(end);
+
+        // Update prevMarker
+        if (prevMarker == end) {
+          prevMarker = prevWaypoint;
+        }
+
+        // Delete marker and polylines from map
+        map.removeLayer(start);
+        start.options.connectedLines.forEach((line) => {
+          if (route.hasLayer(line)) {
+            route.removeLayer(line);
+            removeItemOnce(routeArr, line);
+            totalDistance -= line.getDistance();
+          }
+        });
+        map.removeLayer(end);
+        end.options.connectedLines.forEach((line) => {
+          if (route.hasLayer(line)) {
+            route.removeLayer(line);
+            removeItemOnce(routeArr, line);
+            totalDistance -= line.getDistance();
+          }
+        });
       });
-    });
+  }, 100);
 }
